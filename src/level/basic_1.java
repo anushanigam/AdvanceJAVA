47050a89c04129c1f1274d69d734e7125d21d3dd
divyamarora92 <51014772+divyamarora92@users.noreply.github.com>
2020-10-01 08:45:12 +0530

    Update basic_1.java

--- src/level/basic_1.java
+++ src/level/basic_1.java
@@ -17,7 +17,7 @@ public class basic_1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Enter any string");
+        System.out.println("Enter the string");
         String s = sc.nextLine();
         System.out.println(s);
         
