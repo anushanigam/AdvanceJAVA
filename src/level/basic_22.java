44bdea50e994223ea8827d1402889a151e7384a9
divyamarora92 <51014772+divyamarora92@users.noreply.github.com>
2020-10-01 08:54:04 +0530

    Update basic_22.java

--- src/level/basic_22.java
+++ src/level/basic_22.java
@@ -12,6 +12,7 @@ import java.util.*;
 public class basic_22 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
         String s=sc.nextLine();
         s=s+" ";
         String S="";
